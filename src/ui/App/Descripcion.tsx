b8f01d99f5df1b868922641d95582cfce0bdad30
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-12 15:30:59 -0500

    Actualizar el color de fondo del componente Descripcion para mejorar la estética

--- src/ui/App/Descripcion.tsx
+++ src/ui/App/Descripcion.tsx
@@ -3,7 +3,7 @@ export default function Descripcion() {
     <div className="flex flex-col items-center flex-wrap bg-[#F8F6F1]" >
         <h1 className="bg-[#F4A7C1] text-center font-bold text-2xl p-3 w-full">Descripcion</h1>
         <div className="flex flex-wrap flex-col md:flex-row sm:justify-center">
-            <div className="m-4 p-2 shadow-lg rounded-4xl hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105 bg-[#DCCBB3] hover:bg-[#9794f6] max-w-[250px]">
+            <div className="m-4 p-2 shadow-lg rounded-4xl hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105 bg-[#AEC6CF] hover:bg-[#9794f6] max-w-[250px]">
                 <p className="text-justify max-w-[700px] text-sm py-5 px-3 font-mono ">
                 Postres 100% artesanales🩷 <br />hechos con ingredientes de calidad y un sabor único.
                 </p>
