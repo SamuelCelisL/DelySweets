f2b7be8ff76a0643c75b99d2523f0863c2f342d3
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-12 15:58:41 -0500

    Actualizar el enlace del video en el componente Video

--- src/ui/App/Video.tsx
+++ src/ui/App/Video.tsx
@@ -4,7 +4,7 @@ export default function Video(){
         {/* <h1 className="bg-[#F4A7C1] text-center font-bold text-2xl p-3 w-full">Video</h1> */}
         <iframe 
         className="w-full h-[500px] max-w-[700px]" 
-        src="https://www.youtube.com/embed/MPkSITpHpfQ" 
+        src="https://www.youtube.com/embed/-XDV5fhzuks" 
         title="YouTube video player" 
         // frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
